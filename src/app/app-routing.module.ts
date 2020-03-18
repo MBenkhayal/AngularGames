13e9a8c312a08d9891b2f61838a9eb3c6c291abd
MBenkhayal <m_benkhayal@ymail.com>
2020-03-18 17:19:45 -0500

    fixed routing between app and games list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TicTacToeComponent } from './games/tic-tac-toe/tic-tac-toe.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+import { GamesListComponent } from './games-list/games-list.component';
 
 
 const routes: Routes = [
-  { path: 'tictactoe', component: TicTacToeComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'gameslist', component: GamesListComponent },
+  { path: 'gameslist/tictactoe', component: TicTacToeComponent },
+  { path: '', redirectTo: '/gameslist', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 

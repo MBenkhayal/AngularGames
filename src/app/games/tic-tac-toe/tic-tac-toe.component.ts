2c78ad3399045155b3e49561ab4d0d868e205d85
MBenkhayal <m_benkhayal@ymail.com>
2020-03-21 15:08:10 -0500

    updated buttons, onclick handling, and game status being set

--- src/app/games/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/games/tic-tac-toe/tic-tac-toe.component.ts
@@ -26,14 +26,21 @@ export class TicTacToeComponent implements OnInit {
     this.gameStatus = 'X turn';
   }
 
-  onButtonClick(event, row, col) {
+  onButtonClick(button, row, col) {
+    if(button.textContent != '') { //think about this vs just disabling the button
+      return;
+    }
+
     if(this.playerTurn === 'x') {
       this.gameBoard[row][col] = 'x';
-      this.gameStatus = 'O turn';
+      button.textContent = 'x';
+      // button.disabled=true;
+      this.gameStatus = `Player o's turn`;
       this.playerTurn = 'o';
     } else if(this.playerTurn ==='o') {
       this.gameBoard[row][col] = 'o';
-      this.gameStatus = 'X turn';
+      button.textContent = 'o';
+      this.gameStatus = `Player x's turn`;
       this.playerTurn = 'x';
     }
     this.checkBoard();
@@ -51,6 +58,8 @@ export class TicTacToeComponent implements OnInit {
         this.gameBoard[0][0] == this.gameBoard[1][1] && this.gameBoard[0][0] == this.gameBoard[2][2] ||
         this.gameBoard[2][0] == this.gameBoard[1][1] && this.gameBoard[2][0] == this.gameBoard[0][2]) {
           //set all buttons to be disabled, game ends, declare winner
+          let winner = 'x' ? this.playerTurn === 'o' : 'o';
+          this.gameStatus = `The winner is ${winner}!`;
         }
   }
 }

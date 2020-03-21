f0163addead9b2b0673424862cbc4595d2021bf6
MBenkhayal <m_benkhayal@ymail.com>
2020-03-21 01:07:56 -0500

    began working on logic for game

--- src/app/games/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/games/tic-tac-toe/tic-tac-toe.component.ts
@@ -6,13 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-tac-toe.component.scss']
 })
 export class TicTacToeComponent implements OnInit {
+  playerTurn = '';
+  gameStatus = '';
+  gameBoard = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeGame();
   }
 
-  onButtonClick(event) {
-    console.log(event)
+  initializeGame() {
+    this.gameBoard = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ];
+    this.playerTurn = 'x';
+    this.gameStatus = 'X turn';
+  }
+
+  onButtonClick(event, row, col) {
+    if(this.playerTurn === 'x') {
+      this.gameBoard[row][col] = 'x';
+      this.gameStatus = 'O turn';
+      this.playerTurn = 'o';
+    } else if(this.playerTurn ==='o') {
+      this.gameBoard[row][col] = 'o';
+      this.gameStatus = 'X turn';
+      this.playerTurn = 'x';
+    }
+    this.checkBoard();
+  }
+
+  checkBoard() {
+    if(this.gameBoard[0][0] == this.gameBoard[0][1] && this.gameBoard[0][0] == this.gameBoard[0][2] ||
+        this.gameBoard[1][0] == this.gameBoard[1][1] && this.gameBoard[1][0] == this.gameBoard[1][2] ||
+        this.gameBoard[2][0] == this.gameBoard[2][1] && this.gameBoard[2][0] == this.gameBoard[2][2] ||
+
+        this.gameBoard[0][0] == this.gameBoard[1][0] && this.gameBoard[0][0] == this.gameBoard[2][0] ||
+        this.gameBoard[0][1] == this.gameBoard[1][1] && this.gameBoard[0][1] == this.gameBoard[2][1] ||
+        this.gameBoard[0][2] == this.gameBoard[1][2] && this.gameBoard[0][2] == this.gameBoard[2][2] ||
+
+        this.gameBoard[0][0] == this.gameBoard[1][1] && this.gameBoard[0][0] == this.gameBoard[2][2] ||
+        this.gameBoard[2][0] == this.gameBoard[1][1] && this.gameBoard[2][0] == this.gameBoard[0][2]) {
+          //set all buttons to be disabled, game ends, declare winner
+        }
   }
 }

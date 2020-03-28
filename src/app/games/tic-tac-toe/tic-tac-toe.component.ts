113a0e671c8db148120f61d666bbf597df128dfb
MBenkhayal <m_benkhayal@ymail.com>
2020-03-28 12:47:40 -0500

    added format on save, added scss class for buttons

--- src/app/games/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/games/tic-tac-toe/tic-tac-toe.component.ts
@@ -27,18 +27,18 @@ export class TicTacToeComponent implements OnInit {
   }
 
   onButtonClick(button, row, col) {
-    if(button.textContent != '') { //think about this vs just disabling the button
+    if (button.textContent != '') { //think about this vs just disabling the button
       return;
     }
 
-    if(this.playerTurn === 'x') {
+    if (this.playerTurn === 'x') {
       this.gameBoard[row][col] = 'x';
       button.textContent = 'x';
       button.disabled = true;
       this.gameStatus = `Player o's turn`;
       this.playerTurn = 'o';
       this.checkBoard('x');
-    } else if(this.playerTurn ==='o') {
+    } else if (this.playerTurn === 'o') {
       this.gameBoard[row][col] = 'o';
       button.textContent = 'o';
       button.disabled = true;
@@ -50,18 +50,18 @@ export class TicTacToeComponent implements OnInit {
   }
 
   checkBoard(lastPlayed) {
-    if(this.gameBoard[0][0] == lastPlayed && this.gameBoard[0][1] == lastPlayed &&  this.gameBoard[0][2] == lastPlayed ||
-        this.gameBoard[1][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed &&  this.gameBoard[1][2] == lastPlayed ||
-        this.gameBoard[2][0] == lastPlayed && this.gameBoard[2][1] == lastPlayed &&  this.gameBoard[2][2] == lastPlayed ||
+    if (this.gameBoard[0][0] == lastPlayed && this.gameBoard[0][1] == lastPlayed && this.gameBoard[0][2] == lastPlayed ||
+      this.gameBoard[1][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed && this.gameBoard[1][2] == lastPlayed ||
+      this.gameBoard[2][0] == lastPlayed && this.gameBoard[2][1] == lastPlayed && this.gameBoard[2][2] == lastPlayed ||
 
-        this.gameBoard[0][0] == lastPlayed && this.gameBoard[1][0] == lastPlayed &&  this.gameBoard[2][0] == lastPlayed ||
-        this.gameBoard[0][1] == lastPlayed && this.gameBoard[1][1] == lastPlayed &&  this.gameBoard[2][1] == lastPlayed ||
-        this.gameBoard[0][2] == lastPlayed && this.gameBoard[1][2] == lastPlayed &&  this.gameBoard[2][2] == lastPlayed ||
+      this.gameBoard[0][0] == lastPlayed && this.gameBoard[1][0] == lastPlayed && this.gameBoard[2][0] == lastPlayed ||
+      this.gameBoard[0][1] == lastPlayed && this.gameBoard[1][1] == lastPlayed && this.gameBoard[2][1] == lastPlayed ||
+      this.gameBoard[0][2] == lastPlayed && this.gameBoard[1][2] == lastPlayed && this.gameBoard[2][2] == lastPlayed ||
 
-        this.gameBoard[0][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed &&  this.gameBoard[2][2] == lastPlayed ||
-        this.gameBoard[2][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed &&  this.gameBoard[0][2] == lastPlayed) {
-          //set all buttons to be disabled, game ends, declare winner
-          this.gameStatus = `The winner is ${lastPlayed}!`;
-        }
+      this.gameBoard[0][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed && this.gameBoard[2][2] == lastPlayed ||
+      this.gameBoard[2][0] == lastPlayed && this.gameBoard[1][1] == lastPlayed && this.gameBoard[0][2] == lastPlayed) {
+      //set all buttons to be disabled, game ends, declare winner
+      this.gameStatus = `The winner is ${lastPlayed}!`;
+    }
   }
 }

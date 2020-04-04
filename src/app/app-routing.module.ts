6f089b19d1440e822777196b7e17b4b571d2fdc2
MBenkhayal <m_benkhayal@ymail.com>
2020-04-04 00:32:17 -0500

    updated routing for new snake component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { TicTacToeComponent } from './games/tic-tac-toe/tic-tac-toe.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { GamesListComponent } from './games-list/games-list.component';
-
+import { SnakeComponent } from './games/snake/snake.component';
 
 const routes: Routes = [
   { path: 'gameslist', component: GamesListComponent },
   { path: 'gameslist/tictactoe', component: TicTacToeComponent },
+  { path: 'gameslist/snake', component: SnakeComponent },
   { path: '', redirectTo: '/gameslist', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GamesListComponent } from './games-list/games-list.component';
 import { TicTacToeComponent } from './games/tic-tac-toe/tic-tac-toe.component';
 import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+import { SnakeComponent } from './games/snake/snake.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GamesListComponent,
     TicTacToeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SnakeComponent
   ],
   imports: [
     BrowserModule,

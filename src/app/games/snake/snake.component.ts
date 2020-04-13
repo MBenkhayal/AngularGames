91c73340fa852ee77a1241032ade2dbdba81efd5
MBenkhayal <m_benkhayal@ymail.com>
2020-04-12 23:31:28 -0500

    snake moves and responds to wasd or arrow key inputs

--- src/app/games/snake/snake.component.ts
+++ src/app/games/snake/snake.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-snake',
@@ -14,6 +14,7 @@ export class SnakeComponent implements OnInit {
   direction = "right";
   snake = [];
   score = 0;
+  cellWidth = 10;
 
   constructor() { }
 
@@ -28,10 +29,69 @@ export class SnakeComponent implements OnInit {
 
     setInterval(() => {
       this.drawBoard();
-    }, 40);
+    }, 400);
   }
 
   drawBoard() {
+    //paint canvas
+    this.context.fillStyle = "white";
+    this.context.fillRect(0, 0, this.width, this.height);
 
+
+    this.moveSnake();
+
+    //paint snake
+    this.snake.forEach(cell => {
+      this.paintCell(cell.x, cell.y)
+    });
+  }
+
+  paintCell(x, y) {
+    this.context.fillStyle = "green";
+    this.context.fillRect(x * this.cellWidth, y * this.cellWidth, this.cellWidth, this.cellWidth);
+    this.context.strokeStyle = "white";
+    this.context.strokeRect(x * this.cellWidth, y * this.cellWidth, this.cellWidth, this.cellWidth);
+  }
+
+  moveSnake() {
+    var xHead = this.snake[0].x;
+    var yHead = this.snake[0].y;
+
+    switch (this.direction) {
+      case "right":
+        xHead++;
+        break;
+      case "left":
+        xHead--;
+        break;
+      case "up":
+        yHead--;
+        break;
+      case "down":
+        yHead++;
+        break;
+    }
+
+    var tail = this.snake.pop();
+    tail.x = xHead;
+    tail.y = yHead;
+    this.snake.unshift(tail);
+  }
+
+  @HostListener('document:keydown', ["$event"])
+  handleKeydown(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if ((key == 37 || key == 65) && this.direction != "right") {
+      this.direction = "left";
+    }
+    else if ((key == 38 || key == 87) && this.direction != "down") {
+      this.direction = "up";
+    }
+    else if ((key == 39 || key == 68) && this.direction != "left") {
+      this.direction = "right";
+    }
+    else if ((key == 40 || key == 83) && this.direction != "up") {
+      this.direction = "down";
+    }
   }
 }

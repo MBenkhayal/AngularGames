0eccfa17798c5f2d37a14105b15f52af10bcf485
MBenkhayal <m_benkhayal@ymail.com>
2020-04-21 23:09:47 -0500

    re organized routes array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ import { SnakeComponent } from './games/snake/snake.component';
 import { BrickBreakerComponent } from './games/brick-breaker/brick-breaker.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/gameslist', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
   { path: 'gameslist', component: GamesListComponent },
   { path: 'gameslist/tictactoe', component: TicTacToeComponent },
   { path: 'gameslist/snake', component: SnakeComponent },
-  { path: 'gameslist/brickbreaker', component: BrickBreakerComponent }
+  { path: 'gameslist/brickbreaker', component: BrickBreakerComponent },
+  { path: '', redirectTo: '/gameslist', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({

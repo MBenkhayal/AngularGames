21f06044dc2430dc777c4c969e4e81847f944530
MBenkhayal <m_benkhayal@ymail.com>
2020-04-22 00:16:33 -0500

    ball bounces against walls

--- src/app/games/brick-breaker/brick-breaker.component.ts
+++ src/app/games/brick-breaker/brick-breaker.component.ts
@@ -12,10 +12,11 @@ export class BrickBreakerComponent implements OnInit {
   interval: any;
   width: number;
   height: number;
-  xStart: number;
-  yStart: number;
+  xPosition: number;
+  yPosition: number;
   xOffset = 2;
   yOffset = -2;
+  ballRadius = 10;
 
   constructor() { }
 
@@ -23,8 +24,8 @@ export class BrickBreakerComponent implements OnInit {
     this.context = this.canvas.nativeElement.getContext("2d");
     this.width = this.canvas.nativeElement.width;
     this.height = this.canvas.nativeElement.height;
-    this.xStart = this.width / 2;
-    this.yStart = this.height - 50;
+    this.xPosition = this.width / 2;
+    this.yPosition = this.height - 50;
   }
 
   startGame() {
@@ -36,16 +37,25 @@ export class BrickBreakerComponent implements OnInit {
   drawBoard() {
     this.context.clearRect(0, 0, this.width, this.height);
     this.drawBall();
-
-    this.xStart += this.xOffset;
-    this.yStart += this.yOffset;
+    this.checkWallCollision();
+    this.xPosition += this.xOffset;
+    this.yPosition += this.yOffset;
   }
 
   drawBall() {
     this.context.beginPath();
-    this.context.arc(this.xStart, this.yStart, 10, 0, Math.PI * 2);
+    this.context.arc(this.xPosition, this.yPosition, this.ballRadius, 0, Math.PI * 2);
     this.context.fillStyle = "green";
     this.context.fill();
     this.context.closePath();
+  }
+
+  checkWallCollision() {
+    if (this.yPosition + this.yOffset > this.height - this.ballRadius || this.yPosition + this.yOffset < this.ballRadius) {
+      this.yOffset = - this.yOffset;
+    }
+    if (this.xPosition + this.xOffset > this.width - this.ballRadius || this.xPosition + this.xOffset < this.ballRadius) {
+      this.xOffset = - this.xOffset;
+    }
   }
 }

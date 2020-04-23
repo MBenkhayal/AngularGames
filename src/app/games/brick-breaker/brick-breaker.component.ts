5c09786ac5ba7e6bd1c5ea86fe6d88b6b8ad8935
MBenkhayal <m_benkhayal@ymail.com>
2020-04-22 19:04:29 -0500

    paddle is created and moving left and right, does not go off screen

--- src/app/games/brick-breaker/brick-breaker.component.ts
+++ src/app/games/brick-breaker/brick-breaker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-brick-breaker',
@@ -17,6 +17,11 @@ export class BrickBreakerComponent implements OnInit {
   xOffset = 2;
   yOffset = -2;
   ballRadius = 10;
+  paddleHeight = 10;
+  paddleWidth = 100;
+  paddlePosition: number;
+  movingRight = false;
+  movingLeft = false;
 
   constructor() { }
 
@@ -26,6 +31,7 @@ export class BrickBreakerComponent implements OnInit {
     this.height = this.canvas.nativeElement.height;
     this.xPosition = this.width / 2;
     this.yPosition = this.height - 50;
+    this.paddlePosition = (this.width - this.paddleWidth) / 2;
   }
 
   startGame() {
@@ -36,8 +42,11 @@ export class BrickBreakerComponent implements OnInit {
 
   drawBoard() {
     this.context.clearRect(0, 0, this.width, this.height);
+
     this.drawBall();
     this.checkWallCollision();
+    this.drawPaddle();
+
     this.xPosition += this.xOffset;
     this.yPosition += this.yOffset;
   }
@@ -50,12 +59,54 @@ export class BrickBreakerComponent implements OnInit {
     this.context.closePath();
   }
 
+  drawPaddle() {
+    this.context.beginPath();
+    this.context.rect(this.paddlePosition, this.height - this.paddleHeight, this.paddleWidth, this.paddleHeight);
+    this.context.fillStyle = "blue";
+    this.context.fill();
+    this.context.closePath();
+
+    if (this.movingLeft) {
+      this.paddlePosition -= 10;
+      if (this.paddlePosition < 0) {
+        this.paddlePosition = 0;
+      }
+    } else if (this.movingRight) {
+      this.paddlePosition += 10;
+      if (this.paddlePosition + this.paddleWidth > this.width) {
+        this.paddlePosition = this.width - this.paddleWidth;
+      }
+    }
+  }
+
   checkWallCollision() {
     if (this.yPosition + this.yOffset > this.height - this.ballRadius || this.yPosition + this.yOffset < this.ballRadius) {
       this.yOffset = - this.yOffset;
     }
     if (this.xPosition + this.xOffset > this.width - this.ballRadius || this.xPosition + this.xOffset < this.ballRadius) {
       this.xOffset = - this.xOffset;
+    }
+  }
+
+  @HostListener('document:keydown', ["$event"])
+  handleKeydown(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if ((key == 37 || key == 65)) {
+      this.movingLeft = true;
+    }
+    else if ((key == 39 || key == 68)) {
+      this.movingRight = true;
+    }
+  }
+
+  @HostListener('document:keyup', ["$event"])
+  handleKeyup(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if ((key == 37 || key == 65)) {
+      this.movingLeft = false;
+    }
+    else if ((key == 39 || key == 68)) {
+      this.movingRight = false;
     }
   }
 }

52227ae456674d9951ab03262ba04d1ca7947b91
MBenkhayal <m_benkhayal@ymail.com>
2020-04-25 00:25:56 -0500

    added custom ship, generating on canvas

--- src/app/games/asteroids/asteroids.component.ts
+++ src/app/games/asteroids/asteroids.component.ts
@@ -1,3 +1,5 @@
+// Spaceship source: https://opengameart.org/content/spaceship-8
+
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
@@ -12,6 +14,7 @@ export class AsteroidsComponent implements OnInit {
   gameOn = false;
   width: number;
   height: number;
+  spaceship = new Image();
 
   constructor() { }
 
@@ -19,7 +22,10 @@ export class AsteroidsComponent implements OnInit {
     this.context = this.canvas.nativeElement.getContext("2d");
     this.width = this.canvas.nativeElement.width;
     this.height = this.canvas.nativeElement.height;
+    this.spaceship.src = '../../../assets/spiked ship 3. small.blue_.png';
   }
 
-
+  startGame() {
+    this.context.drawImage(this.spaceship, this.width / 2 - 25, this.height / 2 - 25, 50, 50);
+  }
 }

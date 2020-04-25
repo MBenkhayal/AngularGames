6c965ace993a9056b27b40690922d80d82905ee8
MBenkhayal <m_benkhayal@ymail.com>
2020-04-25 00:28:06 -0500

    added listeners for key presses left and right

--- src/app/games/asteroids/asteroids.component.ts
+++ src/app/games/asteroids/asteroids.component.ts
@@ -1,6 +1,6 @@
 // Spaceship source: https://opengameart.org/content/spaceship-8
 
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-asteroids',
@@ -15,6 +15,8 @@ export class AsteroidsComponent implements OnInit {
   width: number;
   height: number;
   spaceship = new Image();
+  turningLeft = false;
+  turningRight = false;
 
   constructor() { }
 
@@ -27,5 +29,29 @@ export class AsteroidsComponent implements OnInit {
 
   startGame() {
     this.context.drawImage(this.spaceship, this.width / 2 - 25, this.height / 2 - 25, 50, 50);
+  }
+
+
+
+  @HostListener('document:keydown', ["$event"])
+  handleKeydown(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if ((key == 37 || key == 65)) {
+      this.turningLeft = true;
+    }
+    else if ((key == 39 || key == 68)) {
+      this.turningRight = true;
+    }
+  }
+
+  @HostListener('document:keyup', ["$event"])
+  handleKeyup(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if ((key == 37 || key == 65)) {
+      this.turningLeft = false;
+    }
+    else if ((key == 39 || key == 68)) {
+      this.turningRight = false;
+    }
   }
 }

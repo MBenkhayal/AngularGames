c5e9a857ca7b105a93a7e2c0a5a5c0913c623c20
MBenkhayal <m_benkhayal@ymail.com>
2020-04-24 23:44:29 -0500

    set up asteroids canvas

--- src/app/games/asteroids/asteroids.component.ts
+++ src/app/games/asteroids/asteroids.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-asteroids',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asteroids.component.scss']
 })
 export class AsteroidsComponent implements OnInit {
+  @ViewChild("asteroidsCanvas", { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+
+  context: any;
+  gameOn = false;
+  width: number;
+  height: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.context = this.canvas.nativeElement.getContext("2d");
+    this.width = this.canvas.nativeElement.width;
+    this.height = this.canvas.nativeElement.height;
   }
+
 
 }

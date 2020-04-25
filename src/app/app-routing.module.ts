0aedfc60104305188f98e7849f344a2a52c9cc20
MBenkhayal <m_benkhayal@ymail.com>
2020-04-24 23:23:03 -0500

    init asteroids component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { GamesListComponent } from './games-list/games-list.component';
 import { SnakeComponent } from './games/snake/snake.component';
 import { BrickBreakerComponent } from './games/brick-breaker/brick-breaker.component';
+import { AsteroidsComponent } from './games/asteroids/asteroids.component';
 
 const routes: Routes = [
   { path: 'gameslist', component: GamesListComponent },
   { path: 'gameslist/tictactoe', component: TicTacToeComponent },
   { path: 'gameslist/snake', component: SnakeComponent },
   { path: 'gameslist/brickbreaker', component: BrickBreakerComponent },
+  { path: 'gameslist/asteroids', component: AsteroidsComponent },
   { path: '', redirectTo: '/gameslist', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];

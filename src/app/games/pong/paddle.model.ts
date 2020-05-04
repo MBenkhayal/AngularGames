929e6c5856b6c416c363bfb0aaa8cfbb280e5992
MBenkhayal <m_benkhayal@ymail.com>
2020-05-04 17:22:21 -0500

    user paddle moves up and down

--- src/app/games/pong/paddle.model.ts
+++ src/app/games/pong/paddle.model.ts
@@ -3,14 +3,16 @@ export class Paddle {
     y: number;
     width: number;
     height: number;
+    position: number;
     color: string;
     direction: string;
 
-    initPaddle(x: number) {
+    initPaddle(x: number, canvasHeight: number) {
         this.x = x;
         this.y = 250;
         this.width = 10;
         this.height = 75;
+        this.position = canvasHeight - this.y - (this.height / 2);
         this.color = "white";
         this.direction = "";
     }
--- src/app/games/pong/pong.component.ts
+++ src/app/games/pong/pong.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { Paddle } from './paddle.model';
 
 @Component({
@@ -14,17 +14,17 @@ export class PongComponent implements OnInit {
   width = 750;
   height = 500;
   gameOn = false;
-  paddles = [];
+  paddles = []; //0 is player, 1 is computer
 
   constructor() { }
 
   ngOnInit(): void {
     this.context = this.canvas.nativeElement.getContext("2d");
     var temp = new Paddle();
-    temp.initPaddle(0);
+    temp.initPaddle(0, this.height);
     this.paddles.push(temp);
     temp = new Paddle();
-    temp.initPaddle(this.width - 10);
+    temp.initPaddle(this.width - 10, this.height);
     this.paddles.push(temp);
   }
 
@@ -45,10 +45,36 @@ export class PongComponent implements OnInit {
   drawPaddles() {
     this.paddles.forEach(paddle => {
       this.context.beginPath();
-      this.context.rect(paddle.x, this.height - paddle.y - (paddle.height / 2), paddle.width, paddle.height);
+      this.context.rect(paddle.x, paddle.position, paddle.width, paddle.height);
       this.context.fillStyle = paddle.color;
       this.context.fill();
       this.context.closePath();
+
+      if (paddle.direction == "up") {
+        paddle.y += 3;
+        paddle.position = this.height - paddle.y - (paddle.height / 2);
+      } else if (paddle.direction == "down") {
+        paddle.y -= 3;
+        paddle.position = this.height - paddle.y - (paddle.height / 2);
+      }
     });
+  }
+
+  @HostListener('document:keydown', ["$event"])
+  handleKeydown(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if (key == 38 || key == 87) { //up
+      this.paddles[0].direction = "up";
+    } else if (key == 40 || key == 83) { //down
+      this.paddles[0].direction = "down";
+    }
+  }
+
+  @HostListener('document:keyup', ["$event"])
+  handleKeyup(event: KeyboardEvent) {
+    var key = event.keyCode;
+    if (key == 38 || key == 87 || key == 40 || key == 83) {
+      this.paddles[0].direction = "";
+    }
   }
 }

0daa8e22141f8426004e50008adff08c88e4bf13
MBenkhayal <m_benkhayal@ymail.com>
2020-06-17 20:33:48 -0500

    basic setup for brickjumper

--- src/app/games/brick-jumper/brick-jumper.component.ts
+++ src/app/games/brick-jumper/brick-jumper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-brick-jumper',
@@ -6,13 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brick-jumper.component.scss']
 })
 export class BrickJumperComponent implements OnInit {
+  @ViewChild('brickJumperCanvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+
+  context: any;
+  interval: any;
+  intervalSpeed: number;
+  width: number;
+  height: number;
+  gameOn = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.context = this.canvas.nativeElement.getContext('2d');
+    this.width = this.canvas.nativeElement.width;
+    this.height = this.canvas.nativeElement.height;
   }
 
   startGame() {
+    this.gameOn = true;
+    this.interval = setInterval(() => {
+      this.drawBoard();
+    }, this.intervalSpeed);
+  }
+
+  drawBoard() {
+    this.context.clearRect(0, 0, this.width, this.height);
 
   }
 }
